067b4e81cdf830d18da476703610ef4e69d592f7
Emma Tosch <etosch@gmail.com>
2013-03-24 18:31:42 -0400

    moved closures to global; they can now be parameterized

--- Cmd.h
+++ Cmd.h
@@ -1,10 +1,16 @@
+#include <string>
+
 class Cmd{
  public:
   const static int num_cmds = 18;
   static const char * cmds[num_cmds];
   static const int parse_types[num_cmds];
   int cmd;
-  char * cmd_args[];
+  int num_args;
+  struct cmd_node {
+    std::string cmd;
+    cmd_node * next;
+  } cmd_args;
 
   Cmd(int cmd){
     this->cmd=cmd;
--- Parser.cpp
+++ Parser.cpp
@@ -7,6 +7,7 @@
 #include "Bucket.h"
 // Bucket.h includes string.h
 #include <iostream>
+#include <functional>
 
 using namespace std;
 
@@ -14,64 +15,142 @@ const char * Cmd::cmds[] = {"add","append","cas","decr","delete"
 			    ,"flush_all","get","gets","incr","prepend"
 			    ,"quit","replace","set","slabs","stats"
 			    ,"touch","verbosity", "version"};
+
 const int Cmd::parse_types[] = {1,1,1,4,3,8,2,2,4,1,9,1,1,6,7,5,10,9};
 
-Cmd * Parser::parse_storage_cmd(Cmd * c, int bufsize, char buffer[]){
-  char key[KEY_SIZE+1]; int i = strlen(Cmd::cmds[c->cmd]); int j;
-  char flags[6]; char exptime[11]; char bytes[11]; char casunique[20];
-  // advance i until it points at a non-control/whitespace char
-  auto advance = [&] () { 
-    do{ i++; } while ( i<bufsize && (int)buffer[i]<33 );
+void cmd_parse_err (Cmd * c) {
+  cout << "Insufficient arguments to the " << Cmd::cmds[c->cmd] << " command." << endl;
+  exit(-1);
+}
+
+auto advance_fn = [] (int &i, int bufsize, char buffer[]) {
+  return [&i,bufsize,buffer] ()->int {
+    do{ 
+      if (!(int)buffer[i] || (buffer[i]=='\r' && buffer[i+1]=='\n')) {
+	return 0;
+      } else i++; 
+    } while ( i<bufsize && (int)buffer[i]<33 );
+    return 1;    
   };
-  // read key until whitespace
-  auto read = [&] (char target[], int bound, char err_msg[]){
-    j=0;
+};
+
+auto read_fn = [] (int &i, char bufsize, char buffer[]){
+  return [&i,bufsize,buffer] (char target[], int bound, char err_msg[])->void{
+    int j = 0;
     for( ; (int)buffer[i]>32 ; j++, i++){
       if (j>bound){ cout << err_msg  << endl; exit(-1); }
       if (i==bufsize){ 
 	cout << "Buffer too small (" << i; 
-	cout<< "); increase size in Server.cpp?" << endl; exit(-1);}
+	cout << "); increase size in Server.cpp?" << endl; exit(-1);}
       target[j]=buffer[i];
     }
-    target[j+1]='\0';
+    target[j]='\0';
   };
+};
+
+auto add_node = [] (Cmd::cmd_node &initial_node) {
+  return [&] (char s[]){
+    static Cmd::cmd_node * last_ref = &initial_node;
+    last_ref->cmd = string(s);
+    last_ref->next = new Cmd::cmd_node();
+    last_ref = last_ref->next;
+  };
+};
+
+Cmd * Parser::parse_storage_cmd(Cmd * c, int bufsize, char buffer[]){
+  int i = strlen(Cmd::cmds[c->cmd]); int num_args;
+  char key[KEY_SIZE+1]; char flags[6]; char exptime[11]; char bytes[11]; 
+  char casunique[20]; char noreply[8];
+  auto advance = advance_fn(i, bufsize, buffer);
+  auto read = read_fn(i, bufsize, buffer);
   advance(); read(key, KEY_SIZE, "Key too big!");
   advance(); read(flags, 5, "32-bit flags not supported.");
   advance(); read(exptime, 10, "System time must fit in an int.");
   advance(); read(bytes, 10, "Size of data block must fit in an int.");
   if (c->cmd == 2){
-    advance(); read(casunique, 19, "Unique value must fit in a 64-bit int.");
-  }
-  // handle noreply later
-  c->cmd_args[0]=key; c->cmd_args[1]=flags; c->cmd_args[2]=exptime; c->cmd_args[3]=bytes;
-  if (c->cmd == 2) c->cmd_args[4]=casunique;
+    advance(); read(casunique, 19, "Unique value must fit in a 64-bit int.");}
+  if ((int)buffer[i]>32) { 
+    advance(); read(noreply, 7, "To use noreply optional argument, type \"noreply\"");}
+  num_args = 4;
+  auto add = add_node(c->cmd_args);
+  add(key); add(flags); add(exptime); add(bytes);
+  if (c->cmd == 2) { add(casunique); num_args++; } 
+  c->num_args=num_args;
   return c;
 }
+
 Cmd * Parser::parse_retrieval_cmd(Cmd * c, int bufsize, char buffer[]){
+  int i = strlen(Cmd::cmds[c->cmd]);
+  int num_args = 0;
+  Cmd::cmd_node *cmdp = &c->cmd_args;
+  auto advance = [&] () {
+    do {
+      if (buffer[i]=='\r' && buffer[i+1]=='\n'){
+  	if (num_args == 0) {
+  	  cout << Cmd::cmds[c->cmd] << " requires at least one key." << endl;
+  	  exit(-1);
+  	} else {
+  	  c->num_args=num_args;
+  	  return 0;
+  	}
+      } else i++;
+    } while ( i < bufsize && (int)buffer[i]<33 );
+    return 1;
+  };
+  auto read_key = [&] () {
+    char key[KEY_SIZE]; int j;
+    for ( j = 0 ; (int)buffer[i]>32 ; j++, i++ ) {
+      if (j==KEY_SIZE+1) { cout << "Key size too big!" << endl; exit(-1); }
+      key[j]=buffer[i];
+    }
+    key[j]='\0';
+    num_args++;
+    cmdp->cmd = string(key);
+    cmdp->next = new Cmd::cmd_node();
+    cmdp = cmdp->next;
+  };
+  while (advance()) read_key();
   return c;
 }
+
 Cmd * Parser::parse_deletion_cmd(Cmd * c, int bufsize, char buffer[]){
+  c->num_args=0;
+  
   return c;
 }
 Cmd * Parser::parse_cr_cmd(Cmd * c, int bufsize, char buffer[]){
+  c->num_args=0;
+  // c->cmd_arg[0]="0";
   return c;
 }
 Cmd * Parser::parse_touch_cmd(Cmd * c, int bufsize, char buffer[]){
+  c->num_args=0;
+  // c->cmd_arg[0]="0";
   return c;
 }
 Cmd * Parser::parse_slabs_cmd(Cmd * c, int bufsize, char buffer[]){
+  c->num_args=0;
+  // c->cmd_arg[0]="0";
   return c;
 }
 Cmd * Parser::parse_stats_cmd(Cmd * c, int bufsize, char buffer[]){
+  c->num_args=0;
+  // c->cmd_arg[0]="0";
   return c;
 }
 Cmd * Parser::parse_flush_cmd(Cmd * c, int bufsize, char buffer[]){
+  c->num_args=0;
+  // c->cmd_arg[0]="0";
   return c;
 }
 Cmd * Parser::parse_singleton(Cmd * c, int bufsize, char buffer[]){
+  c->num_args=0;
+  // c->cmd_arg[0]="0";
   return c;
 }
 Cmd * Parser::parse_verbosity(Cmd * c, int bufsize, char buffer[]){
+  c->num_args=0;
+  // c->cmd_arg[0]="0";
   return c;
 }
 
@@ -96,7 +175,6 @@ Cmd * Parser::parse(int bufsize, char buffer[]){
 Cmd * Parser::parse_cmd(int bufsize, char buffer[]){
   int cmd,i,j;
   for(cmd=-1, i=0, j=0 ; j<Cmd::num_cmds && i<bufsize ; i++){
-    //    cout << buffer[i] << "(" << (int) buffer[i]<< ")," << Cmd::cmds[j] << endl;
     if (buffer[i] < 33) //commands are whitespace delimited
       return new Cmd(cmd);
     else if (buffer[i]!=Cmd::cmds[j][i]){
@@ -116,17 +194,22 @@ bool Parser::test(){
   auto printtest = [] (char s[]) {
     Cmd * c = Parser::parse(512, s);
     int index = c->cmd;
-    const char * this_cmd = (index<0)?"not found":Cmd::cmds[index];
-    cout << "Command: " << this_cmd << "\tArgs: ";
-    for (int i=0; c->cmd_args[i]!='\0' ; i++)
-      cout << c->cmd_args[i];
+    int num_args = c->num_args;
+    Cmd::cmd_node this_cmd = c->cmd_args;
+    const char * this_cmd_name = (index<0)?"not found":Cmd::cmds[index];
+    cout << "Command: " << this_cmd_name << "\tArgs(" << num_args << "): ";
+    for ( ; num_args > 0 ; num_args--){
+      cout << this_cmd.cmd  << "\t";
+      this_cmd=*this_cmd.next;
+    }
     cout << endl;
+    delete c;
   };
-  printtest("add ;lkasjdf 123 ");
+  printtest("add ;lkasjdf 123 3845706987 938475");
   printtest("touch");
   printtest("flush_all\r\n");
-  printtest("get\tstuff");
-  printtest("version");
+  printtest("get\tstuff more_stuff\r\n");
+  printtest("version\r\n");
   printtest("laksjdfha");
   return true;
 }
